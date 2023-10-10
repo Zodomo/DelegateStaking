3bc986fb7d9e5c7e655c5967a01041fffa3e12f9
Zodomo <zodomo@proton.me>
2023-10-09 20:32:10 -0500

    init setup

---
+++ test/DelegateStaking.t.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {console2} from "forge-std/Test.sol";
+import "solmate/test/utils/DSTestPlus.sol";
+import "../src/DelegateStaking.sol";
+
+contract DelegateStakingTest is DSTestPlus {
+
+}
